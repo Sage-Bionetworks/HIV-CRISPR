1200b05977b58637449b88bf6d2561748643c398
lopierra <34149564+lopierra@users.noreply.github.com>
2021-08-10 18:14:10 -0700

    modify get_synapse_data.R to pull all output data upfront

--- get_synapse_data.R
+++ get_synapse_data.R
@@ -1,11 +1,12 @@
 library(yaml)
 
 # log in to Synapse
-# don't use this if using the Shiny Synapse Template with session token
+# (don't do this if using the Shiny Synapse Template with session token)
 
 # synLogin()
 
-## DATA THAT APPLIES TO ALL COMPARISONS
+
+## GET LIBRARY & GENECARDS DATA
 
 # get the libraries folder
 libraries_raw <- syncFromSynapse("syn21915617")
@@ -36,51 +37,63 @@ CUL3_GO_GC <- CUL3_metascape.csv %>%
   mutate(genecards = paste0("<a href='https://www.genecards.org/cgi-bin/carddisp.pl?id_type=entrezgene&id=", `Gene ID`, "' target='_blank'>", `Gene Symbol`, "</a>"),
          .after = `Gene ID`)
 
+
+## GET METADATA FOR ALL SCREENS
+
 # Get MAGeCK Output View with metadata for all output folders
 metadata <- read_csv(synTableQuery(sprintf("SELECT * FROM syn25976216", "syn25976216"))$filepath) %>% 
   select(-starts_with("ROW"))
 
 # Get File View linking sample sheets to output files using configId
-output_view <- read_csv(synTableQuery(sprintf("SELECT * FROM syn25435509", 
-                                              "syn25435509"))$filepath) %>% 
-  select(-starts_with("ROW"))
+# output_view <- read_csv(synTableQuery(sprintf("SELECT * FROM syn25435509", 
+#                                               "syn25435509"))$filepath) %>% 
+#   select(-starts_with("ROW"))
 
 
-## DATA FOR SINGLE COMPARISON
+## GET OUTPUT DATA FOR ALL SCREENS
 
-# function to pull info, counts files, and output files and return everything as a list
-# example: syn25435417
-get_info_counts_outputs <- function(sample_sheet_id){
-  sample_sheet <- unlist(read_yaml(synGet(sample_sheet_id)$path))
+# function to get data for one screen
+
+get_screen_data <- function(folder_id){
+  
+  folder_name <- metadata %>% 
+    filter(id == folder_id) %>%
+    pull(name)
+    
+  syncFromSynapse(folder_id) %>% 
+    map_chr("path") %>% 
+    set_names(paste0(folder_name, "_", basename(.))) %>% 
+    map(read_tsv) %>% 
+    list2env(., .GlobalEnv)
+}
+
+# now map to all screens
+
+metadata %>% 
+  filter(!is.na(configId)) %>%
+  pull(id) %>% 
+  map(get_screen_data) %>% 
+
+
+## GET COUNT FILES FOR SINGLE SCREEN
+
+get_counts <- function(configId){
+  sample_sheet <- unlist(read_yaml(synGet(configId)$path))
   
   info_counts_list <- list(library_name = sample_sheet["comparisons.library_name"],
-       screen_name = sample_sheet["comparisons.comparison_name"],
-       treatment1_id = sample_sheet["comparisons.treatment_synapse_ids1"],
-       treatment1 = read_tsv(synGet(unname(sample_sheet["comparisons.treatment_synapse_ids1"]))$path),
-       treatment2 = read_tsv(synGet(unname(sample_sheet["comparisons.treatment_synapse_ids2"]))$path),
-       control1 = read_tsv(synGet(unname(sample_sheet["comparisons.control_synapse_ids1"]))$path),
-       control2 = read_tsv(synGet(unname(sample_sheet["comparisons.control_synapse_ids2"]))$path))
+                           screen_name = sample_sheet["comparisons.comparison_name"],
+                           treatment1_id = sample_sheet["comparisons.treatment_synapse_ids1"],
+                           treatment1 = read_tsv(synGet(unname(sample_sheet["comparisons.treatment_synapse_ids1"]))$path),
+                           treatment2 = read_tsv(synGet(unname(sample_sheet["comparisons.treatment_synapse_ids2"]))$path),
+                           control1 = read_tsv(synGet(unname(sample_sheet["comparisons.control_synapse_ids1"]))$path),
+                           control2 = read_tsv(synGet(unname(sample_sheet["comparisons.control_synapse_ids2"]))$path))
   
-  joined_list <- list(treatment_joined = left_join(info_counts_list$treatment1,
+  list(treatment_joined = left_join(info_counts_list$treatment1,
                                                    info_counts_list$treatment2, 
                                                    by = "sgRNA"),
                       control_joined = left_join(info_counts_list$control1, 
                                                  info_counts_list$control2,
-                                                 by = "sgRNA"))
-  # get output files as a list
-  output_list <- output_view %>% 
-    filter(configId == sample_sheet_id) %>% 
-    pull(id) %>% 
-    syncFromSynapse() %>% 
-    map_chr("path") %>% 
-    set_names(basename(.)) %>% 
-    map(read_tsv)
-  
-  # put everything into a single list, flatten out the output list
-  results_list <- flatten(list(info_counts_list, 
-                               joined_list, 
-                               output_list))
-  
-  return(results_list)
-  
+                                                 by = "sgRNA")) %>% 
+    list2env(., .GlobalEnv)
 }
+
